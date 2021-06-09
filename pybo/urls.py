546300b534e353de1f70d1774eab3f040c3cb23b
Hope <gmlakd5944@naver.com>
2021-06-09 16:10:15 +0900

    6-09mid

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name='index'),
+    path('<int:question_id>/',views.detail, name='detail'),
 ]
